a9beaac7cb643afe370c12ebe6bfb7faed9bc381
maikolguerrero <maikolguerreop@gmail.com>
2024-07-06 20:21:33 -0400

    Agregar Paginación y modal, y algunas mejoras

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,154 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import Sidebar from './components/Sidebar';
+// import Dashboard from './views/Dashboard';
+// import Payroll from './views/Payroll';
+// import Employees from './views/Employees';
+// import Departments from './views/Departments';
+// import Positions from './views/Positions';
+// import Reports from './views/Reports';
+// import PerceptionsDeductions from './views/PerceptionsDeductions';
+// import Attendances from './views/Attendances';
+// import Banks from './views/Banks';
+// import Settings from './views/Settings';
+// import CompanySettings from './views/CompanySettings';
+// import UserProfile from './views/UserProfile';
+// import Users from './views/admin/Users';
+// import paths from './config/routePaths';
+// import {
+//   FiMenu,
+// } from "react-icons/fi";
+
+// export default function App() {
+//   const [isSidebarOpen, setSidebarOpen] = useState(true);
+
+//   const toggleSidebar = () => {
+//     setSidebarOpen(!isSidebarOpen);
+//   };
+
+//   return (
+//     <div className="flex h-screen bg-principalAzul">
+//       {/* Botón de menú hamburguesa fuera del sidebar */}
+//       <button
+//         className={`text-white p-2 focus:outline-none top-4 left-4 z-50 md:hidden ${isSidebarOpen ? 'hidden' : 'fixed'}`}
+//         onClick={toggleSidebar}
+//       >
+//         <FiMenu className="w-4 h-4" />
+//       </button>
+
+//       <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+
+//       <main
+//         className={`flex-1 p-4 overflow-y-auto transition-transform transform ${isSidebarOpen ? 'ml-56' : 'ml-0'} transition-all`}
+//       >
+//         <Routes>
+//           <Route index element={<Dashboard />} />
+//           <Route path={paths.DASHBOARD_PATH} element={<Dashboard />} />
+//           <Route path={paths.PAYROLL_PATH} element={<Payroll />} />
+//           <Route path={paths.EMPLOYEES_PATH} element={<Employees />} />
+//           <Route path={paths.DEPARTMENTS_PATH} element={<Departments />} />
+//           <Route path={paths.POSITIONS_PATH} element={<Positions />} />
+//           <Route path={paths.REPORTS_PATH} element={<Reports />} />
+//           <Route path={paths.PERCEPTIONS_DEDUCTIONS_PATH} element={<PerceptionsDeductions />} />
+//           <Route path={paths.ATTENDANCES_PATH} element={<Attendances />} />
+//           <Route path={paths.BANKS_PATH} element={<Banks />} />
+//           <Route path={paths.SETTINGS_PATH} element={<Settings />} />
+//           <Route path={paths.COMPANY_SETTINGS_PATH} element={<CompanySettings />} />
+//           <Route path={paths.USER_PROFILE_PATH} element={<UserProfile />} />
+//           <Route path={paths.USERS_PATH} element={<Users />} />
+//           <Route path="*" element={<h1>Error 404</h1>} />
+//         </Routes>
+//       </main>
+//     </div>
+//   );
+// }
+
+
+// import React, { useState, useEffect } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import Sidebar from './components/Sidebar';
+// import Dashboard from './views/Dashboard';
+// import Payroll from './views/Payroll';
+// import Employees from './views/Employees';
+// import Departments from './views/Departments';
+// import Positions from './views/Positions';
+// import Reports from './views/Reports';
+// import PerceptionsDeductions from './views/PerceptionsDeductions';
+// import Attendances from './views/Attendances';
+// import Banks from './views/Banks';
+// import Settings from './views/Settings';
+// import CompanySettings from './views/CompanySettings';
+// import UserProfile from './views/UserProfile';
+// import Users from './views/admin/Users';
+// import paths from './config/routePaths';
+// import { FiMenu } from "react-icons/fi";
+
+// export default function App() {
+//   const [isSidebarOpen, setSidebarOpen] = useState(window.innerWidth >= 768);
+
+//   const toggleSidebar = () => {
+//     setSidebarOpen(!isSidebarOpen);
+//   };
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       if (window.innerWidth >= 768) {
+//         setSidebarOpen(true);
+//       } else {
+//         setSidebarOpen(false);
+//       }
+//     };
+
+//     // Set initial state based on window size
+//     handleResize();
+
+//     // Add event listener for window resize
+//     window.addEventListener('resize', handleResize);
+
+//     // Cleanup event listener on component unmount
+//     return () => window.removeEventListener('resize', handleResize);
+//   }, []);
+
+//   return (
+//     <div className="flex h-screen bg-principalAzul">
+//       {/* Botón de menú hamburguesa fuera del sidebar */}
+//       <button
+//         className={`text-white p-1 focus:outline-none z-50 md:hidden ${isSidebarOpen ? 'hidden' : 'fixed'}`}
+//         onClick={toggleSidebar}
+//       >
+//         <FiMenu className="w-6 h-6" />
+//       </button>
+
+//       <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+
+//       <main
+//         className={`flex-1 p-4 overflow-y-auto transition-transform transform ${isSidebarOpen ? 'ml-56' : 'ml-0'} transition-all`}
+//       >
+//         <Routes>
+//           <Route index element={<Dashboard />} />
+//           <Route path={paths.DASHBOARD_PATH} element={<Dashboard />} />
+//           <Route path={paths.PAYROLL_PATH} element={<Payroll />} />
+//           <Route path={paths.EMPLOYEES_PATH} element={<Employees />} />
+//           <Route path={paths.DEPARTMENTS_PATH} element={<Departments />} />
+//           <Route path={paths.POSITIONS_PATH} element={<Positions />} />
+//           <Route path={paths.REPORTS_PATH} element={<Reports />} />
+//           <Route path={paths.PERCEPTIONS_DEDUCTIONS_PATH} element={<PerceptionsDeductions />} />
+//           <Route path={paths.ATTENDANCES_PATH} element={<Attendances />} />
+//           <Route path={paths.BANKS_PATH} element={<Banks />} />
+//           <Route path={paths.SETTINGS_PATH} element={<Settings />} />
+//           <Route path={paths.COMPANY_SETTINGS_PATH} element={<CompanySettings />} />
+//           <Route path={paths.USER_PROFILE_PATH} element={<UserProfile />} />
+//           <Route path={paths.USERS_PATH} element={<Users />} />
+//           <Route path="*" element={<h1>Error 404</h1>} />
+//         </Routes>
+//       </main>
+//     </div>
+//   );
+// }
+
+
+
+import React, { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Dashboard from './views/Dashboard';
@@ -15,31 +165,48 @@ import CompanySettings from './views/CompanySettings';
 import UserProfile from './views/UserProfile';
 import Users from './views/admin/Users';
 import paths from './config/routePaths';
-import {
-  FiMenu,
-} from "react-icons/fi";
+import { FiMenu } from "react-icons/fi";
 
 export default function App() {
-  const [isSidebarOpen, setSidebarOpen] = useState(true);
+  const [isSidebarOpen, setSidebarOpen] = useState(window.innerWidth >= 768);
 
   const toggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen);
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setSidebarOpen(true);
+      } else {
+        setSidebarOpen(false);
+      }
+    };
+
+    // Set initial state based on window size
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className="flex h-screen bg-principalAzul">
       {/* Botón de menú hamburguesa fuera del sidebar */}
       <button
-        className={`text-white p-2 focus:outline-none top-4 left-4 z-50 md:hidden ${isSidebarOpen ? 'hidden' : 'fixed'}`}
+        className={`text-white p-1 focus:outline-none z-50 md:hidden ${isSidebarOpen ? 'hidden' : 'fixed'}`}
         onClick={toggleSidebar}
       >
-        <FiMenu className="w-4 h-4" />
+        <FiMenu className="w-6 h-6" />
       </button>
 
       <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
 
       <main
-        className={`flex-1 p-4 overflow-y-auto transition-transform transform ${isSidebarOpen ? 'ml-56' : 'ml-0'} transition-all`}
+        className={`flex-1 p-4 overflow-y-auto transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'ml-56' : 'ml-0'} md:ml-56`}
       >
         <Routes>
           <Route index element={<Dashboard />} />
