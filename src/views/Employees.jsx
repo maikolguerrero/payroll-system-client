68723ced7e9dfdfe03b205e4bdce9855e4c99c55
Joshua-desings <susanow189@gmail.com>
2024-07-22 07:25:31 -0400

    Update Employees

--- src/views/Employees.jsx
+++ src/views/Employees.jsx
@@ -1,39 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import Modal from '../components/Modal';
-import ConfirmDeleteModal from '../components/ConfirmDeleteModal';
-import AddButton from '../components/AddButton';
-import SearchBar from '../components/SearchBar';
-import Pagination from '../components/Pagination';
-import Table from '../components/Table';
-import FormEmployees from '../components/forms/FormEmployees';
-
-const employeesData = [
-  { id: 1, ci: "12345678", name: "John Doe", department: "IT", position: "Developer", salary: "$5000", startDate: "2020-01-15", phone: "123456789", email: "john.doe@example.com", address: "123 Main St", birthDate: "1990-05-12" },
-  { id: 2, ci: "09876543", name: "Jane Smith", department: "HR", position: "Manager", salary: "$6000", startDate: "2019-02-20", phone: "987654321", email: "jane.smith@example.com", address: "456 Elm St", birthDate: "1985-07-23" },
-];
+import React, { useState, useEffect, useContext } from "react";
+import Modal from "../components/Modal";
+import ConfirmDeleteModal from "../components/ConfirmDeleteModal";
+import AddButton from "../components/AddButton";
+import SearchBar from "../components/SearchBar";
+import Pagination from "../components/Pagination";
+import Table from "../components/Table";
+import FormEmployees from "../components/forms/FormEmployees";
+import { Contexto } from "../context/Contexto";
+import { alertConfirm, alertError } from "../components/alerts/alerts";
 
 const columns = [
   { label: "CI", accessor: "ci" },
   { label: "Nombre", accessor: "name" },
-  { label: "Departamento", accessor: "department" },
-  { label: "Cargo", accessor: "position" },
-  { label: "Salario", accessor: "salary" },
-  { label: "Fecha Ingreso", accessor: "startDate" },
+  { label: "Departamento", accessor: "department_id" },
+  { label: "Cargo", accessor: "position_id" },
+  { label: "Salario", accessor: "base_salary" },
+  { label: "Fecha Ingreso", accessor: "hire_date" },
   { label: "Teléfono", accessor: "phone" },
   { label: "Correo", accessor: "email" },
   { label: "Dirección", accessor: "address" },
-  { label: "Fecha de Nacimiento", accessor: "birthDate" },
+  { label: "Fecha de Nacimiento", accessor: "birthdate" },
 ];
 
 const ITEMS_PER_PAGE = 10;
 
 const Employees = () => {
-  const [employees, setEmployees] = useState(employeesData);
-  const [filteredEmployees, setFilteredEmployees] = useState(employeesData);
+  const { employeesData, setEmployeesData, setDepartmentsData, setPositionsData, peticionGet, peticionDelete } = useContext(Contexto);
+
+  const [employees, setEmployees] = useState([]);
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [currentEmployee, setCurrentEmployee] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [employeesResponse, departmentsResponse, positionsResponse] = await Promise.all([
+          peticionGet("http://localhost:3000/api/employees/all", "GET"),
+          peticionGet("http://localhost:3000/api/departments/all", "GET"),
+          peticionGet("http://localhost:3000/api/positions/all", "GET")
+        ]);
+        setEmployeesData(employeesResponse);
+        setEmployees(employeesResponse);
+        setDepartmentsData(departmentsResponse);
+        setPositionsData(positionsResponse);
+      } catch (error) {
+        console.error("Error fetching data", error);
+      }
+    };
+
+    fetchData();
+  }, [peticionGet, setEmployeesData, setDepartmentsData, setPositionsData]);
+
+  useEffect(() => {
+    setFilteredEmployees(employees);
+  }, [employees]);
 
   const openAddModal = () => {
     setCurrentEmployee(null);
@@ -58,24 +81,33 @@ const Employees = () => {
     setIsDeleteModalOpen(false);
   };
 
-  const handleDelete = () => {
-    setEmployees(employees.filter(emp => emp.id !== currentEmployee.id));
-    setFilteredEmployees(filteredEmployees.filter(emp => emp.id !== currentEmployee.id));
+  const handleDelete = async () => {
+    try {
+      await peticionDelete(`http://localhost:3000/api/employees/${currentEmployee._id}`, "DELETE");
+      const updatedEmployees = employees.filter((emp) => emp._id !== currentEmployee._id);
+      setEmployees(updatedEmployees);
+      setFilteredEmployees(updatedEmployees);
+      alertConfirm("Empleado eliminado exitosamente");
+    } catch (error) {
+      alertError("Ocurrio un Error Revisa la Consola");
+    }
     closeDeleteModal();
   };
 
   const handleSearch = (query) => {
-    const filtered = employees.filter(employee =>
-      employee.ci.toLowerCase().includes(query.toLowerCase()) ||
-      employee.name.toLowerCase().includes(query.toLowerCase()) ||
-      employee.department.toLowerCase().includes(query.toLowerCase()) ||
-      employee.position.toLowerCase().includes(query.toLowerCase()) ||
-      employee.salary.toLowerCase().includes(query.toLowerCase()) ||
-      employee.startDate.toLowerCase().includes(query.toLowerCase()) ||
-      employee.phone.toLowerCase().includes(query.toLowerCase()) ||
-      employee.email.toLowerCase().includes(query.toLowerCase()) ||
-      employee.address.toLowerCase().includes(query.toLowerCase()) ||
-      employee.birthDate.toLowerCase().includes(query.toLowerCase())
+    const lowercasedQuery = query.toLowerCase();
+    const filtered = employees.filter(
+      (employee) =>
+        employee.ci.toLowerCase().includes(lowercasedQuery) ||
+        employee.name.toLowerCase().includes(lowercasedQuery) ||
+        employee.department.toLowerCase().includes(lowercasedQuery) ||
+        employee.position.toLowerCase().includes(lowercasedQuery) ||
+        employee.salary.toLowerCase().includes(lowercasedQuery) ||
+        employee.startDate.toLowerCase().includes(lowercasedQuery) ||
+        employee.phone.toLowerCase().includes(lowercasedQuery) ||
+        employee.email.toLowerCase().includes(lowercasedQuery) ||
+        employee.address.toLowerCase().includes(lowercasedQuery) ||
+        employee.birthDate.toLowerCase().includes(lowercasedQuery)
     );
     setFilteredEmployees(filtered);
     setCurrentPage(1);
@@ -92,35 +124,58 @@ const Employees = () => {
 
   const handleAddEditEmployee = (employeeData) => {
     if (currentEmployee) {
-      setEmployees(employees.map(emp => emp.id === currentEmployee.id ? employeeData : emp));
-      setFilteredEmployees(filteredEmployees.map(emp => emp.id === currentEmployee.id ? employeeData : emp));
+      const updatedEmployees = employees.map((emp) =>
+        emp.id === currentEmployee.id ? employeeData : emp
+      );
+      setEmployees(updatedEmployees);
+      setFilteredEmployees(updatedEmployees);
     } else {
       const newEmployee = { ...employeeData, id: employees.length + 1 };
-      setEmployees([...employees, newEmployee]);
-      setFilteredEmployees([...filteredEmployees, newEmployee]);
+      const updatedEmployees = [...employees, newEmployee];
+      setEmployees(updatedEmployees);
+      setFilteredEmployees(updatedEmployees);
     }
     closeModal();
   };
 
   return (
-    <div className="p-4 dark:bg-gray-900">
-      <h1 className="text-2xl text-white dark:text-gray-100 font-bold mb-4 text-left">Empleados</h1>
+    <div className="p-4 bg-white dark:bg-gray-900 text-gray-800 dark:text-white min-h-screen">
+      <h1 className="text-2xl font-bold mb-4 text-left">
+        Empleados
+      </h1>
       <div className="flex flex-col sm:flex-row justify-between items-start mb-4 gap-2 sm:gap-0">
         <SearchBar placeholder="Buscar empleados..." onSearch={handleSearch} />
       </div>
-      <Table columns={columns} data={currentItems} onEdit={openEditModal} onDelete={openDeleteModal} />
-      <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={handlePageChange}
-      />
+      {employees.length === 0 ? (
+        <h3 className="text-2xl font-bold mt-8 mb-4 text-center">
+          No hay empleados registrados...
+        </h3>
+      ) : (
+        <>
+          <Table
+            columns={columns}
+            data={currentItems}
+            onEdit={openEditModal}
+            onDelete={openDeleteModal}
+          />
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={handlePageChange}
+          />
+        </>
+      )}
       <AddButton openModal={openAddModal} />
       <Modal
         isOpen={isModalOpen}
         onClose={closeModal}
         title={currentEmployee ? "Editar empleado" : "Agregar Empleado"}
       >
-        <FormEmployees employee={currentEmployee} onSubmit={handleAddEditEmployee} />
+        <FormEmployees
+          employee={currentEmployee}
+          onSubmit={handleAddEditEmployee}
+          onClose={closeModal}
+        />
       </Modal>
       <ConfirmDeleteModal
         isOpen={isDeleteModalOpen}
